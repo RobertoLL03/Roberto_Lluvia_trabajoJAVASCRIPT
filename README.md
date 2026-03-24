1915fb88a2793e883894ed5c12f9cf76e777632b
RobertoLL03 <rlluviajim7869@gmail.com>
2026-03-24 22:07:27 +0100

    Add files via upload

---
+++ README.md
@@ -0,0 +1,60 @@
+# XANA
+
+
+<img src="./assets/images/xana_logo.jpg" width="30%">
+
+
+XANA es una empresa de tecnología, sobre todo de informática, que combina artículos de actualidad con curiosidades de la misma. Aquí encontrarás información de utilidad, noticias actuales, venta de nuestros productos, una galería de imágenes, etc., y todo ello relacionado con la informática.
+
+El tren sólo pasa una vez. Aprovecha :)
+
+
+
+## Instrucciones de uso
+
+
+
+Para acceder y poder disfrutar del contenido es muy sencillo. Primero que nada debes acceder a la página haciendo clic en el siguiente enlace: [https://robertoll03.github.io/Roberto\_Lluvia\_trabajoJAVASCRIPT/](https://robertoll03.github.io/Roberto_Lluvia_trabajoJAVASCRIPT/). 
+
+Tienes varias secciones:
+
+
+
+* Noticias
+* Productos
+* Galería de imágenes
+* Información de nuestros colaboradores
+* Apartado o sección para conocer mejor XANA
+* Y un sinfín de cosas más
+
+
+
+Para ver los contenidos de los productos y la galería tan solo tienes que pulsar en las opciones de la barra de navegación con sus respectivos nombres:<br><br>
+
+<img src="./assets/images/instruccion_readme_barra_navegacion.png"><br><br>
+
+Para elegir y adquirir los productos, tienes la opción de Ir al presupuesto en la parte baja de la página de dichos productos:<br><br>
+
+<img src="./assets/images/instruccion_readme_boton_ir_a_presupuesto.png"><br><br>
+
+Desde el presupuesto, tienes que rellenar los datos que te pide y después elegir el producto que quieras. También puedes seleccionar los extras. De ahí te marcará el precio total de dicho presupuesto.
+
+Puedes obtener un descuento que se aplicará al precio total. Se realiza en base al plazo de días de entrega que introduzcas:
+
+
+
+* Si introduces de 4 a 7 días, se te aplicará un descuento del 5%.
+* Si introduces 8 días o más, el descuento será del 10%.<br><br>
+
+
+
+<img src="./assets/images/instruccion_readme_descuento_presupuesto.png">
+
+## Direcciones de repositorio y hosting de GitHub
+
+
+
+Repositorio: [https://github.com/RobertoLL03/Roberto\_Lluvia\_trabajoJAVASCRIPT](https://github.com/RobertoLL03/Roberto_Lluvia_trabajoJAVASCRIPT)
+
+Hosting: [https://robertoll03.github.io/Roberto\_Lluvia\_trabajoJAVASCRIPT/](https://robertoll03.github.io/Roberto_Lluvia_trabajoJAVASCRIPT/)
+
---
+++ js/script_mapa.js
@@ -0,0 +1,84 @@
+let options = {
+    enableHighAccuracy: true,
+    timeout: 5000,
+    maximumAge: 0
+}
+
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(success, error, options);
+} else {
+    alert('Los servicios de geolocalización no están disponibles.');
+}
+
+function success(position) {
+    let latitude = position.coords.latitude;
+    let longitude = position.coords.longitude;
+
+    let map = L.map('map', {
+        center: [latitude, longitude],
+        zoom: 17
+    });
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+    // Definir iconos
+
+    let inicio = L.icon({
+        iconUrl: '../assets/images/punto_ubicacion_verde.png',
+        shadowUrl: '../assets/images/punto_ubicacion_sombra.png',
+        iconSize: [38, 95],
+        shadowSize: [50, 64],
+        iconAnchor: [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor: [-3, -90]
+    })
+
+    let final = L.icon({
+        iconUrl: '../assets/images/punto_ubicacion_rojo.png',
+        shadowUrl: '../assets/images/punto_ubicacion_sombra.png',
+        iconSize: [38, 95],
+        shadowSize: [50, 64],
+        iconAnchor: [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor: [-3, -90]
+    })
+
+    let track = L.icon({
+        iconUrl: '../assets/images/punto_ubicacion_naranja.png',
+        shadowUrl: '../assets/images/punto_ubicacion_sombra.png',
+        iconSize: [38, 95],
+        shadowSize: [50, 64],
+        iconAnchor: [22, 94],
+        shadowAnchor: [4, 62],
+        popupAnchor: [-3, -90]
+    })
+
+    // Calcular ruta
+
+    let control = L.Routing.control({
+        waypoints: [
+            L.latLng(latitude, longitude),
+            L.latLng(40.959834, -5.657975)
+        ],
+        language: 'es',
+        createMarker: function(i, wp, nWps) {
+            switch(i) {
+                case 0:
+                    return L.marker(wp.latLng, {icon: inicio, draggable: true}).bindPopup('Inicio (estoy aquí)');
+                case nWps - 1:
+                    return L.marker(wp.latLng, {icon: final, draggable: true}).bindPopup('Destino');
+                default:
+                    return L.marker(wp.latLng, {icon: track, draggable: true}).bindPopup('Paso intermedio');
+            }
+        }
+    }).addTo(map);
+}
+
+function error() {
+    let map = L.map('map', {
+        center: [40.959834, -5.657975],
+        zoom: 17
+    });
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+}
